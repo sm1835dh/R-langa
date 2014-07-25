95287c7910e6f6f0845c63a2ab751d27e8c38c86
sm1835dh <1000630@SKP1000630MN001.local>
2014-07-26 00:43:14 +0900

    Initial Commit

---
+++ R+langa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  R+langa
+//
+//  Created by sm1835dh on 2014. 7. 26..
+//  Copyright (c) 2014년 sm1835dh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ R+langaTests/R_langaTests.swift
@@ -0,0 +1,36 @@
+//
+//  R_langaTests.swift
+//  R+langaTests
+//
+//  Created by sm1835dh on 2014. 7. 26..
+//  Copyright (c) 2014년 sm1835dh. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class R_langaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
